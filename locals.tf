f49c2d011a851dc0e5698e8e9482dada81b0059f
hrmny <github.leah@hrmny.sh>
2023-07-04 05:20:43 +0200

    fmt

--- locals.tf
+++ locals.tf
@@ -2,8 +2,8 @@ locals {
   users = {
     hrmny = {
       ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEDtuIourWzbibkXqsJYk8IHxl4SIvxmnttrlUALu8XS hrmny@c-base.org"
-	}
-	saces = {
+    }
+    saces = {
       ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5JruwHRLLVW1LDcoUymftOgqt6I2khSvvSnNLk7XZ4 saces@c-base.org"
     }
   }
