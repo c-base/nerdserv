2a45ffb9f41a3176bbb2d901ec33bd915021f3ea
Uwe Kamper <me@uwekamper.de>
2023-08-29 22:32:41 +0200

    add new VM for cbtelbot (#7)

---
+++ proxmox/cbtelbot.tf
@@ -0,0 +1,28 @@
+module "cbtelbot_vm" {
+  source = "../modules/vm"
+  node   = var.node
+  pool   = var.pool
+
+  name  = "cbtelbot"
+  vm_id = "104"
+
+  clone = data.proxmox_virtual_environment_vm.debian_cloud_vm_template.vm_id
+
+  cores  = 1
+  memory = 1024
+
+  disk = {
+    // gigabytes
+    size    = 48
+    storage = var.storage.disk
+  }
+
+  network = {
+    bridge          = var.network.bridge
+    internal_bridge = proxmox_virtual_environment_network_linux_bridge.internal_bridge.name
+  }
+
+  admins = [
+    local.users["uk"]
+  ]
+}
