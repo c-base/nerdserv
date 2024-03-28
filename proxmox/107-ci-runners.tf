f05cdc2f4da59b8294e65ecc8995e50b3da12b95
hrmny <8845940+ForsakenHarmony@users.noreply.github.com>
2024-03-28 04:58:47 +0100

    feat: add 2 VMs for saces

---
+++ proxmox/107-ci-runners.tf
@@ -0,0 +1,29 @@
+module "ci_runners_vm" {
+  source = "../modules/vm"
+  node   = var.node
+  pool   = var.pool
+
+  name  = "ci-runners"
+  vm_id = "107"
+
+  clone = data.proxmox_virtual_environment_vm.debian_cloud_vm_template.vm_id
+
+  cores  = 4
+  memory = 4096
+  weight = 128
+
+  disk = {
+    // gigabytes
+    size    = 32
+    storage = var.storage.disk
+  }
+
+  network = {
+    bridge          = var.network.bridge
+    internal_bridge = proxmox_virtual_environment_network_linux_bridge.internal_bridge.name
+  }
+
+  admins = [
+    local.users["saces"]
+  ]
+}
