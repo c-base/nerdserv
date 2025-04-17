41649ef65c6a7b86156edfb7de413029db154c68
Uwe Kamper <me@uwekamper.de>
2025-04-18 01:47:55 +0200

    add VM with minimum TrueNAS requirements: https://www.truenas.com/docs/scale/22.12/gettingstarted/scalehardwareguide/

---
+++ proxmox/113-truenas-test.tf
@@ -0,0 +1,33 @@
+module "truenast_test_vm" {
+  source = "../modules/vm"
+  node   = var.node
+  pool   = var.pool
+
+  name  = "saces-misc-services"
+  vm_id = "113"
+
+  clone = data.proxmox_virtual_environment_vm.debian_cloud_vm_template.vm_id
+
+  cores  = 2
+  memory = 8096
+
+  disks = [
+    {
+      size_gb = 20
+      storage = var.storage.disk
+    },
+    {
+      size_gb = 128
+      storage = var.storage.disk
+    },
+  ]
+
+  network = {
+    bridge          = var.network.bridge
+    internal_bridge = proxmox_virtual_environment_network_linux_bridge.internal_bridge.name
+  }
+
+  admins = [
+    local.users["uk"]
+  ]
+}
